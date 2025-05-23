bc648d874c1b366a26bfc20120ee10bc8c492bbd
Dev-Yadnesh8 <142681172+Dev-Yadnesh8@users.noreply.github.com>
2025-05-23 14:29:35 +0530

    custom api response and error handling added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
-import  connectToDB  from "./db/db.js";
+import connectToDB from "./db/db.js";
+import { app } from "./app.js";
 
-
-connectToDB();
+connectToDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(` SERVER RUNNING ON PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("SERVER CONNECTION FAILED !!", err);
+  });
