72792a8dc6a7b2180690ed5f620bd76e9cfdf087
Dev-Yadnesh8 <142681172+Dev-Yadnesh8@users.noreply.github.com>
2025-05-26 12:19:04 +0530

    added refresh-tokens endpoint

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,23 +3,21 @@ import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
 import UserValidator from "../validators/user.validator.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
-const generateAccessAndRefreshTokens = async (userId)=>{
-    try {
-        console.log("User iD" , userId);
-        
-      const user = await User.findById({_id :userId});
-      const accessToken =  user.generateAccessToken();
-      const refreshToken =  user.generateRefreshToken();
-      user.refreshToken = refreshToken;
-      await user.save({validateBeforeSave : false});
-      return {accessToken,refreshToken};
-    } catch (error) {
-        console.log(error);
-        
-        throw new ApiError(500,"Something went wrong while generating tokens");
-    }
-}
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById({ _id: userId });
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    // console.log(error);
+    throw new ApiError(500, "Something went wrong while generating tokens");
+  }
+};
 
 const signUp = asyncHandler(async (req, res) => {
   // Step1: Get data form user & Validate data
@@ -52,7 +50,9 @@ const signUp = asyncHandler(async (req, res) => {
   });
 
   // Step4 : Check if user is created or not & if exist remove password and refersh token field
-  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
   if (!createdUser) {
     throw new ApiError(
       500,
@@ -63,51 +63,124 @@ const signUp = asyncHandler(async (req, res) => {
   // Setp5 : Send successful response
   return res
     .status(201)
-    .json(new ApiResponse(200, "Your account has been created. Let’s get started!", createdUser));
-});
-
-const signIn = asyncHandler(async (req,res)=>{
-    //Step1 : Get data from user and validate it. 
-    const result = UserValidator.validateSignIn(req.body);
-    if(!result.success){
-        throw new ApiError(422,"Validation error",result.errors);
-    }
-
-    //Step2: Check if user existis or not.
-    const {identifier,password} = result.data;
-    const user = await User.findOne({ $or : [{email : identifier},{username:identifier}]});
-    if(!user){
-        throw new ApiError(404,"It looks like you don’t have an account yet. Please sign up.");
-    }
-
-    
-    //Step3 : Validate user credintials with backend credintials & generate tokens.
-    const isPasswordCorrect = await  user.checkPassword(password);
-    if(!isPasswordCorrect){
-        throw new ApiError(401, "Invalid email/username or password. Please try again.");
-    }
-
-    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id.toHexString());
-    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
-
-    //Step4 : Send token & response 
-    const cookieOptions = {
-        httpOnly : true,
-        secure : true
-    }
-   return res.status(200).cookie("accessToken",accessToken,cookieOptions).cookie("refreshToken",refreshToken,cookieOptions).json(
-        new ApiResponse(200,"Login Successful",{accessToken,refreshToken,user :loggedInUser})
+    .json(
+      new ApiResponse(
+        200,
+        "Your account has been created. Let’s get started!",
+        createdUser
+      )
     );
 });
 
-const logoutUser = asyncHandler(async(req,res)=>{
-   await User.findByIdAndUpdate(req.user._id,{$set:{ refreshToken : undefined}},{new : true});
-    const cookieOptions = {
-        httpOnly : true,
-        secure : true
-    }
-   return res.status(200).clearCookie("accessToken",cookieOptions).clearCookie("refreshToken",cookieOptions).json(new ApiResponse(200,"User logout successfully"));
+const signIn = asyncHandler(async (req, res) => {
+  //Step1 : Get data from user and validate it.
+  const result = UserValidator.validateSignIn(req.body);
+  if (!result.success) {
+    throw new ApiError(422, "Validation error", result.errors);
+  }
 
+  //Step2: Check if user existis or not.
+  const { identifier, password } = result.data;
+  const user = await User.findOne({
+    $or: [{ email: identifier }, { username: identifier }],
+  });
+  if (!user) {
+    throw new ApiError(
+      404,
+      "It looks like you don’t have an account yet. Please sign up."
+    );
+  }
 
+  //Step3 : Validate user credintials with backend credintials & generate tokens.
+  const isPasswordCorrect = await user.checkPassword(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(
+      401,
+      "Invalid email/username or password. Please try again."
+    );
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id.toHexString()
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  //Step4 : Send token & response
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .json(
+      new ApiResponse(200, "Login Successful", {
+        accessToken,
+        refreshToken,
+        user: loggedInUser,
+      })
+    );
 });
-export { signUp,signIn,logoutUser };
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    { $set: { refreshToken: undefined } },
+    { new: true }
+  );
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", cookieOptions)
+    .clearCookie("refreshToken", cookieOptions)
+    .json(new ApiResponse(200, "User logout successfully"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const userRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+  console.log(userRefreshToken);
+  if (!userRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+  try {
+    const decodedToken = jwt.verify(
+      userRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "User not found. Invalid refresh token.");
+    }
+
+    if (userRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Refresh token expired or already used");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+    );
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, cookieOptions)
+      .cookie("refreshToken", refreshToken, cookieOptions)
+      .json(
+        new ApiResponse(200, "Tokens refreshed successfully", {
+          accessToken,
+          refreshToken,
+        })
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+});
+export { signUp, signIn, logoutUser, refreshAccessToken };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { logoutUser, signIn, signUp } from "../controllers/user.controller.js";
+import { logoutUser, refreshAccessToken, signIn, signUp } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/sign-up").post(signUp)
 router.route("/sign-in").post(signIn)
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-tokens").post(refreshAccessToken)
 
 export default router;
